782c2f089b0cd4707cf8707f1e922bf26c9948e0
jonathan-weibel-sonarsource <jonathan.weibel@sonarsource.com>
2019-12-12 11:59:10 +0100

    init project with js code smell

---
+++ index.js
@@ -0,0 +1,11 @@
+(function() {
+  let toto = 1;
+  const tutu = "HJJGFJGJHKH";
+  toto = "Jean-Claude Dusse";
+
+  coucou(toto);
+ 
+  function coucou(blabla) {
+    console.log('Coucou,', blabla);
+  }
+})();
